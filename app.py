cb225a8e2a5a9337d878075948d5adb58b21928e
sindani254 <sindani254@gmail.com>
2019-04-19 11:23:07 +0300

    updated templates

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+from functools import wraps
 
 app = Flask(__name__)
+
+app.secret_key = "@nonymous"
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/')
@@ -15,8 +29,21 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'invalid credentials, please try again'
         else:
+            session['logged_in'] = True
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/profile', methods=['GET'])
+@login_required
+def profile():
+    return render_template("profile.html")
 
 
 if __name__ == '__main__':
